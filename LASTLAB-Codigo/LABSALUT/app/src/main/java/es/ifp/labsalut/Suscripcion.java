a7f36c3a6fea3b0cf62572c91b30f0eea79d56f6
shiresp <jose.antonio.moreno.fdez@outlook.com>
2024-04-30 23:40:17 +0200

    Retoques

--- LASTLAB-Codigo/LABSALUT/app/src/main/java/es/ifp/labsalut/Suscripcion.java
+++ LASTLAB-Codigo/LABSALUT/app/src/main/java/es/ifp/labsalut/Suscripcion.java
@@ -1,22 +1,17 @@
 package es.ifp.labsalut;
-import android.os.Bundle;
-import android.widget.Button;
 
 import java.util.Date;
 
 public class Suscripcion {
+
     private String email;
     private boolean esSuscrito;
     private Date finSuscripcion;
-
-    private Button btnCancelarSuscripcion;
-    private Usuario usuario; // Relación con la clase Usuario
 
     public Suscripcion(String email, boolean esSuscrito, Date finSuscripcion, Usuario usuario) {
         this.email = email;
         this.esSuscrito = esSuscrito;
         this.finSuscripcion = finSuscripcion;
-        this.usuario = usuario;
     }
 
     public String getEmail() {
@@ -42,14 +37,4 @@ public class Suscripcion {
     public void setFinSuscripcion(Date finSuscripcion) {
         this.finSuscripcion = finSuscripcion;
     }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-
-
 }
