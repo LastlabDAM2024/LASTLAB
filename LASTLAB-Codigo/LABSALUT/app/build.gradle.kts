4f835c0efe2597b43f351add9aae5468f6259ac9
shiresp <jose.antonio.moreno.fdez@outlook.com>
2024-10-16 23:36:05 +0200

    build.gradle actualizado para claves MAPS

--- LASTLAB-Codigo/LABSALUT/app/build.gradle.kts
+++ LASTLAB-Codigo/LABSALUT/app/build.gradle.kts
@@ -1,5 +1,8 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.secrets_gradle_plugin") version "0.5"
 }
 
 android {
@@ -14,6 +17,27 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // Cargar desde secrets.properties
+        val secretsPropertiesFile = rootProject.file("secrets.properties")
+        if (secretsPropertiesFile.exists()) {
+            val secretsProperties = Properties().apply {
+                load(secretsPropertiesFile.inputStream())
+            }
+            val mapsApiKey: String = secretsProperties.getProperty("MAPS_API_KEY", "")
+            val placesApiKey: String = secretsProperties.getProperty("PLACES_API_KEY", "")
+            manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
+            manifestPlaceholders["PLACES_API_KEY"] = placesApiKey
+            resValue("string", "maps_api_key", mapsApiKey)
+            resValue("string", "places_api_key", placesApiKey)
+        } else {
+            val mapsApiKey: String = System.getenv("MAPS_API_KEY") ?: ""
+            val placesApiKey: String = System.getenv("PLACES_API_KEY") ?: ""
+            manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
+            manifestPlaceholders["PLACES_API_KEY"] = placesApiKey
+            resValue("string", "maps_api_key", mapsApiKey)
+            resValue("string", "places_api_key", placesApiKey)
+        }
     }
 
     buildTypes {
@@ -47,7 +71,14 @@ dependencies {
     implementation(libs.biometric)
     implementation(libs.security.crypto)
     implementation(libs.material.v1120)
-    testImplementation(libs.junit)
+    implementation(libs.places)
+    implementation (libs.viewpager2)
+    implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
+    implementation (libs.github.glide)
+    implementation (libs.material.v150)
+    implementation(libs.activity)
+    testImplementation(libs.testng)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
